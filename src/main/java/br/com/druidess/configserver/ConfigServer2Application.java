19b89dd3adf01d5a44588743040b1ca76ead70b8
Eduardo <eduardomelle@gmail.com>
2022-06-30 20:17:36 -0300

    Commit inicial

---
+++ src/main/java/br/com/druidess/configserver/ConfigServer2Application.java
@@ -0,0 +1,13 @@
+package br.com.druidess.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/druidess/configserver/ConfigServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.druidess.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
